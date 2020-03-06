64f7f95583ab862aa24de8731c8c628381d145b0
KulataevKanat <RaketaEnot4466@gmail.com>
2020-03-06 21:37:28 +0600

    DiscountPlatformApplicationTests

--- src/test/java/kg/DiscountPlatform/DiscountPlatformApplicationTests.java
+++ src/test/java/kg/DiscountPlatform/DiscountPlatformApplicationTests.java
@@ -8,7 +8,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DiscountPlatformApplicationTests {
-
 	@Test
 	public void contextLoads() {
 	}
